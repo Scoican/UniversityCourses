a419b4a9904f2b6c9b6e80f2c381dfcc1a6b7fe8
Scoican Ovidiu <scoican.ovidiu@gmail.com>
2019-04-05 14:21:03 +0300

    Parent-Child problem

---
+++ Database Management Systems/ComicBookStoreApplication/ComicBookStoreApplication/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ComicBookStoreApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowTemplate.Height = 24;
+            this.dataGridViewParent.Size = new System.Drawing.Size(398, 348);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(416, 12);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowTemplate.Height = 24;
+            this.dataGridViewChild.Size = new System.Drawing.Size(372, 348);
+            this.dataGridViewChild.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+    }
+}
+
---
+++ Database Management Systems/ComicBookStoreApplication/ComicBookStoreApplication/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace ComicBookStoreApplication
+{
+
+    public partial class Form1 : Form
+    {
+        BindingSource childBS;
+        private DataSet ds;
+        private SqlDataAdapter childAdapter;
+        private SqlCommandBuilder cb;
+        private string connectionString = "Server=DESKTOP-O7DL3PI\\SQLEXPRESS;Database=ComicBookStore;Integrated Security=true";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            try
+            {
+                ds = new DataSet();
+                conn.Open();
+                SqlDataAdapter parentAdapter = new SqlDataAdapter("SELECT * FROM ComicBooks", conn);
+                childAdapter = new SqlDataAdapter("SELECT * FROM Artists", conn);
+                cb = new SqlCommandBuilder(childAdapter);
+                parentAdapter.Fill(ds, "ComicBooks");
+                childAdapter.Fill(ds, "Artists");
+                BindingSource parentBS = new BindingSource();
+                childBS = new BindingSource();
+                parentBS.DataSource = ds.Tables["ComicBooks"];
+                dataGridViewParent.DataSource = parentBS;
+                DataColumn pk = ds.Tables["ComicBooks"].Columns["id"];
+                DataColumn fk = ds.Tables["Artists"].Columns["id"];
+                DataRelation relation = new DataRelation("FK_ComicBooks_Artists", pk, fk);
+                ds.Relations.Add(relation);
+                childBS.DataSource = parentBS;
+                childBS.DataMember = "FK_ComicBooks_Artists";
+                dataGridViewChild.DataSource = childBS;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void DataGridViewChild_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                childAdapter.Update(ds, "Artists");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void EngineForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                childAdapter.Update(ds, "Artists");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+
+
