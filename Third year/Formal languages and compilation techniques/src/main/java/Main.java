4282cb841f71c11e1e9cd5976f80864acaa3556b
Scoican Ovidiu <scoican.ovidiu@gmail.com>
2019-11-09 17:57:07 +0200

    LFTC Changes

--- Third year/Formal languages and compilation techniques/src/main/java/Main.java
+++ Third year/Formal languages and compilation techniques/src/main/java/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] argv){
         Analyzer analyzer = new Analyzer();
         try {
-            //analyzer.interpret(Constants.INPUT_1,Constants.OUTPUT_1);
+            analyzer.interpret(Constants.INPUT_1,Constants.OUTPUT_1);
             //analyzer.interpret(Constants.INPUT_2,Constants.OUTPUT_2);
-            analyzer.interpret(Constants.INPUT_3,Constants.OUTPUT_3);
+            //analyzer.interpret(Constants.INPUT_3,Constants.OUTPUT_3);
         } catch (Exception e) {
             e.printStackTrace();
         }
